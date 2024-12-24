d705d9073d93378f416fdc8142184cf7f543966b
Lexie <contact@lexedia.moe>
2024-12-24 09:18:53 +0100

    feat: add support for osu! (#128)
    * add support for osu!
    
    * my bad gang
    
    ---------
    
    Co-authored-by: Cynthia <cynthia@cynthia.dev>

--- packages/extension/build/manifest.ts
+++ packages/extension/build/manifest.ts
@@ -85,6 +85,7 @@ export default function manifest (): Plugin {
 							'https://*.discord.com/*',
 							'https://*.github.com/*',
 							'https://*.modrinth.com/*',
+							'https://*.osu.ppy.sh/*',
 							'https://*.sr.ht/*',
 							'https://*.twitch.tv/*',
 							'https://*.twitter.com/*',
